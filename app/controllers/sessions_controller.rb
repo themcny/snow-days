6387b094c54aeed471d4a2ebfdda2f76c369cbef
Andrew Blum & Danielle Cameron & Nicole Yee <pair+axblum.danyell85.themcny@devbootcamp.com>
2015-12-18 10:59:03 -0800

    Debug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ get '/sessions' do
 end
 
 get '/gotogoogle' do
- redirect "https://accounts.google.com/o/oauth2/auth?scope=email%20profile&state=b&redirect_uri=http://snow-days.herokuapp.com/oauthcallback&response_type=code&client_id=#{ENV["GOOGLE_CLIENT_ID"]}"
+ redirect "https://accounts.google.com/o/oauth2/auth?scope=email%20profile&state=b&redirect_uri=#{ENV["HOST"]}/oauthcallback&response_type=code&client_id=#{ENV["GOOGLE_CLIENT_ID"]}"
 end
 
 get '/oauthcallback' do
@@ -13,12 +13,14 @@ get '/oauthcallback' do
     code: params[:code],
     client_id: ENV["GOOGLE_CLIENT_ID"],
     client_secret: ENV["GOOGLE_SECRET"],
-    redirect_uri: "http://snow-days.herokuapp.com/oauthcallback",
+    redirect_uri: "http://#{ENV["HOST"]}/oauthcallback",
     grant_type: "authorization_code"
   }
+  p body
   p body[:redirect_uri]
   post_repsonse = HTTParty.post("https://accounts.google.com/o/oauth2/token", body: body)
   me = HTTParty.get("https://www.googleapis.com/plus/v1/people/me?access_token=#{post_repsonse["access_token"]}")
+  p me
   email = me.parsed_response["emails"].first
   email =email["value"]
   @user = User.find_by(email: email)
