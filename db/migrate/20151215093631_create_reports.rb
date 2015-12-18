4c0ff93d623501e622f2a875a758e7b3aed92bf0
Andrew Blum & Danielle Cameron & Nicole Yee <pair+axblum.danyell85.themcny@devbootcamp.com>
2015-12-18 12:12:22 -0800

    Texts work

--- db/migrate/20151215093631_create_reports.rb
+++ db/migrate/20151215093631_create_reports.rb
@@ -1,7 +1,7 @@
 class CreateReports < ActiveRecord::Migration
   def change
     create_table :reports do |t|
-      t.decimal :snow_allday, :snow_day, :snow_night, :high, :low
+      t.float :snow_allday, :snow_day, :snow_night, :high, :low
       t.datetime :date_epoch
       t.belongs_to :resort
 
